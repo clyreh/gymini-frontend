7b3b550f7b1f3d6b72ddffc97417a6e74e8acf70
Zahrah Rashid <elevennobodies@gmail.com>
2025-06-28 15:47:44 -0400

    fixing routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import LandingPage from "./pages/LandingPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <LandingPage />
+    <App />
   </StrictMode>
 );
